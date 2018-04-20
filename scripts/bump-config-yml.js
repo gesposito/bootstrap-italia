8e7780cdda23a0bbbcec13edee4c20daab1fb508
CircleCI <circleci@circleci>
2018-04-20 17:11:57 +0000

    Updates

--- scripts/bump-config-yml.js
+++ scripts/bump-config-yml.js
@@ -3,14 +3,13 @@ const fs = require("fs");
 
 const path = "_config.yml";
 const encoding = "utf8";
+// package.json is updated by `github.com/zeit/release` or similar
 const version = require("../package.json").version;
-
-const { CIRCLE_TAG } = process.env;
 
 // Get document, or throw exception on error
 try {
   let doc = yaml.safeLoad(fs.readFileSync(path, encoding));
-  doc.current_version = CIRCLE_TAG || version;
+  doc.current_version = version;
   fs.writeFileSync(path, yaml.safeDump(doc), encoding);
 } catch (e) {
   console.error(e);
